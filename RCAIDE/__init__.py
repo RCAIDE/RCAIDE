0e4fecc2131f69d0c24611301006e5bef6ce416c
planes <ebotero@stanford.edu>
2023-06-08 15:49:45 -0700

    RCAIDE number 1!!

---
+++ RCAIDE/__init__.py
@@ -0,0 +1,18 @@
+# RCAIDE/__init__.py
+
+""" RCAIDE Package Setup
+"""
+
+# ----------------------------------------------------------------------------------------------------------------------
+#  IMPORT
+# ----------------------------------------------------------------------------------------------------------------------
+
+from . import Core
+from . import Analyses
+from . import Attributes
+from . import Components
+from . import Energy
+from . import External_Interfaces
+from . import Methods
+from . import Optimization
+from . import Visualization
