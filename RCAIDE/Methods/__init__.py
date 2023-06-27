7149ad3fdb656a8ee8157f8c085d49c4003df958
planes <ebotero@stanford.edu>
2023-06-27 12:34:37 -0700

    init for methods

---
+++ RCAIDE/Methods/__init__.py
@@ -0,0 +1,27 @@
+# RCAIDE/__init__.py
+# Copyright RCAIDE Hall Trust
+
+""" RCAIDE Package Setup
+"""
+
+# ----------------------------------------------------------------------------------------------------------------------
+#  IMPORT
+# ----------------------------------------------------------------------------------------------------------------------
+
+from . import Aerodynamics
+from . import Center_of_Gravity
+from . import Costs
+from . import Flight_Dynamics
+from . import Geometry
+from . import Missions
+from . import Noise
+from . import Performance
+from . import Power
+from . import Propulsion
+from . import Utilities
+from . import Weights
+from . import Cryogenics
+
+
+from Legacy.trunk.S.Methods import skip
+
