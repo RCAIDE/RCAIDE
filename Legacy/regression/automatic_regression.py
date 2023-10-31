b3822811726a118420381975c5163ca3784970e2
Racheal Erhard <rerhard@stanford.edu>
2023-10-31 11:40:26 -0700

    updated automatic regression to run for legacy SUAVE, and use for import to RCAIDE automatic regression

--- Legacy/regression/automatic_regression.py
+++ Legacy/regression/automatic_regression.py
@@ -35,81 +35,81 @@ from Legacy.trunk.S.Core.DataOrdered import DataOrdered
 modules = [
 
     # ----------------------- Regression List --------------------------
-    '../Legacy/regression/scripts/aerodynamics/aerodynamics.py',
-    '../Legacy/regression/scripts/aerodynamics/all_moving_surfaces_vlm.py',
-    '../Legacy/regression/scripts/aerodynamics/control_surfaces_vlm.py',
-    '../Legacy/regression/scripts/aerodynamics/sears_haack.py',
-    '../Legacy/regression/scripts/aerodynamics/sideslip_and_rotation_vlm.py',
-    '../Legacy/regression/scripts/airfoil_import/airfoil_import_test.py',
-    '../Legacy/regression/scripts/airfoil_import/airfoil_interpolation_test.py',
-    '../Legacy/regression/scripts/airfoil_analysis/airfoil_panel_method_test.py', 
-    '../Legacy/regression/scripts/atmosphere/atmosphere.py',
-    '../Legacy/regression/scripts/atmosphere/constant_temperature.py',
-    '../Legacy/regression/scripts/AVL/test_AVL.py',
-    '../Legacy/regression/scripts/B737/mission_B737.py',
-    '../Legacy/regression/scripts/battery/aircraft_discharge_comparisons.py',
-    '../Legacy/regression/scripts/battery/battery_cell_discharge_tests.py',
-    '../Legacy/regression/scripts/cmalpha/cmalpha.py',
-    '../Legacy/regression/scripts/cnbeta/cnbeta.py',
-    '../Legacy/regression/scripts/concorde/concorde.py',
-    '../Legacy/regression/scripts/ducted_fan/ducted_fan_network.py',
-    '../Legacy/regression/scripts/ducted_fan/battery_ducted_fan_network.py',
-    '../Legacy/regression/scripts/ducted_fan/serial_hybrid_ducted_fan_network.py',
-    '../Legacy/regression/scripts/dynamic_stability/dynamicstability.py',
-    '../Legacy/regression/scripts/electric_performance/propeller_single_point.py',
-    '../Legacy/regression/scripts/electric_performance/electric_V_h_diagram.py',
-    '../Legacy/regression/scripts/electric_performance/electric_payload_range.py',
-    '../Legacy/regression/scripts/Embraer_E190_constThr/mission_Embraer_E190_constThr.py',
-    '../Legacy/regression/scripts/fuel_cell/fuel_cell.py',
-    '../Legacy/regression/scripts/gasturbine_network/gasturbine_network.py',
-    '../Legacy/regression/scripts/geometry/NACA_airfoil_compute.py',
-    '../Legacy/regression/scripts/geometry/NACA_volume_compute.py',
-    '../Legacy/regression/scripts/geometry/wing_fuel_volume_compute.py',
-    '../Legacy/regression/scripts/geometry/fuselage_planform_compute.py',
-    '../Legacy/regression/scripts/industrial_costs/industrial_costs.py',
-    '../Legacy/regression/scripts/internal_combustion_propeller/ICE_Test.py',
-    '../Legacy/regression/scripts/internal_combustion_propeller/ICE_CS_Test.py',
-    '../Legacy/regression/scripts/lifting_line/lifting_line.py',
-    '../Legacy/regression/scripts/mission_range_and_weight_sizing/landing_field_length.py',
-    '../Legacy/regression/scripts/mission_range_and_weight_sizing/take_off_field_length.py',
-    '../Legacy/regression/scripts/mission_range_and_weight_sizing/take_off_weight_from_tofl.py',
-    '../Legacy/regression/scripts/motor/motor_test.py',
-    '../Legacy/regression/scripts/multifidelity/optimize_mf.py',
-    '../Legacy/regression/scripts/noise_optimization/Noise_Test.py',
-    '../Legacy/regression/scripts/noise_fidelity_zero/DC_10_noise.py', 
-    '../Legacy/regression/scripts/noise_fidelity_one/propeller_noise.py',
-    '../Legacy/regression/scripts/noise_fidelity_one/aircraft_noise.py',
-    '../Legacy/regression/scripts/nonuniform_propeller_inflow/nonuniform_propeller_inflow.py',
-    '../Legacy/regression/scripts/optimization_packages/optimization_packages.py',
-    '../Legacy/regression/scripts/payload_range/payload_range.py',
-    '../Legacy/regression/scripts/plots/plot_test.py',
-    '../Legacy/regression/scripts/propeller/propeller_test.py',
-    '../Legacy/regression/scripts/propeller_speeds/range_endurance_speeds.py',
-    '../Legacy/regression/scripts/propulsion_surrogate/propulsion_surrogate.py',
-    '../Legacy/regression/scripts/ramjet_network/ramjet_network.py',
-    '../Legacy/regression/scripts/Regional_Jet_Optimization/Optimize2.py',
-    '../Legacy/regression/scripts/scramjet_network/scramjet_network.py',
-    '../Legacy/regression/scripts/rocket_network/Rocketdyne_F1.py',
-    '../Legacy/regression/scripts/rocket_network/Rocketdyne_J2.py',
-    '../Legacy/regression/scripts/segments/segment_test.py',
-    '../Legacy/regression/scripts/segments/transition_segment_test.py',
-    '../Legacy/regression/scripts/slipstream/slipstream_test.py',
-    '../Legacy/regression/scripts/slipstream/propeller_interactions.py',
-    '../Legacy/regression/scripts/solar_network/solar_network.py',
-    '../Legacy/regression/scripts/solar_network/solar_low_fidelity_network.py',
-    '../Legacy/regression/scripts/solar_radiation/solar_radiation.py',
-    '../Legacy/regression/scripts/SU2_surrogate/BWB-450.py',
-    '../Legacy/regression/scripts/sweeps/test_sweeps.py',
-    '../Legacy/regression/scripts/test_input_output/test_xml_read_write.py',
-    '../Legacy/regression/scripts/test_input_output/test_freemind_write.py',
-    '../Legacy/regression/scripts/turboelectric_HTS_ducted_fan_network/turboelectric_HTS_ducted_fan_network.py',
-    '../Legacy/regression/scripts/turboelectric_HTS_dynamo_ducted_fan_network/turboelectric_HTS_dynamo_ducted_fan_network.py',
-    '../Legacy/regression/scripts/variable_cruise_distance/variable_cruise_distance.py',
-    '../Legacy/regression/scripts/V_n_diagram/V_n_diagram_regression.py',
-    '../Legacy/regression/scripts/VTOL/test_Multicopter.py',
-    '../Legacy/regression/scripts/VTOL/test_Tiltwing.py',
-    '../Legacy/regression/scripts/VTOL/test_Stopped_Rotor.py',
-    '../Legacy/regression/scripts/weights/weights.py'
+    '../regression/scripts/aerodynamics/aerodynamics.py',
+    '../regression/scripts/aerodynamics/all_moving_surfaces_vlm.py',
+    '../regression/scripts/aerodynamics/control_surfaces_vlm.py',
+    '../regression/scripts/aerodynamics/sears_haack.py',
+    '../regression/scripts/aerodynamics/sideslip_and_rotation_vlm.py',
+    '../regression/scripts/airfoil_import/airfoil_import_test.py',
+    '../regression/scripts/airfoil_import/airfoil_interpolation_test.py',
+    '../regression/scripts/airfoil_analysis/airfoil_panel_method_test.py', 
+    '../regression/scripts/atmosphere/atmosphere.py',
+    '../regression/scripts/atmosphere/constant_temperature.py',
+    '../regression/scripts/AVL/test_AVL.py',
+    '../regression/scripts/B737/mission_B737.py',
+    '../regression/scripts/battery/aircraft_discharge_comparisons.py',
+    '../regression/scripts/battery/battery_cell_discharge_tests.py',
+    '../regression/scripts/cmalpha/cmalpha.py',
+    '../regression/scripts/cnbeta/cnbeta.py',
+    '../regression/scripts/concorde/concorde.py',
+    '../regression/scripts/ducted_fan/ducted_fan_network.py',
+    '../regression/scripts/ducted_fan/battery_ducted_fan_network.py',
+    '../regression/scripts/ducted_fan/serial_hybrid_ducted_fan_network.py',
+    '../regression/scripts/dynamic_stability/dynamicstability.py',
+    '../regression/scripts/electric_performance/propeller_single_point.py',
+    '../regression/scripts/electric_performance/electric_V_h_diagram.py',
+    '../regression/scripts/electric_performance/electric_payload_range.py',
+    '../regression/scripts/Embraer_E190_constThr/mission_Embraer_E190_constThr.py',
+    '../regression/scripts/fuel_cell/fuel_cell.py',
+    '../regression/scripts/gasturbine_network/gasturbine_network.py',
+    '../regression/scripts/geometry/NACA_airfoil_compute.py',
+    '../regression/scripts/geometry/NACA_volume_compute.py',
+    '../regression/scripts/geometry/wing_fuel_volume_compute.py',
+    '../regression/scripts/geometry/fuselage_planform_compute.py',
+    '../regression/scripts/industrial_costs/industrial_costs.py',
+    '../regression/scripts/internal_combustion_propeller/ICE_Test.py',
+    '../regression/scripts/internal_combustion_propeller/ICE_CS_Test.py',
+    '../regression/scripts/lifting_line/lifting_line.py',
+    '../regression/scripts/mission_range_and_weight_sizing/landing_field_length.py',
+    '../regression/scripts/mission_range_and_weight_sizing/take_off_field_length.py',
+    '../regression/scripts/mission_range_and_weight_sizing/take_off_weight_from_tofl.py',
+    '../regression/scripts/motor/motor_test.py',
+    '../regression/scripts/multifidelity/optimize_mf.py',
+    '../regression/scripts/noise_optimization/Noise_Test.py',
+    '../regression/scripts/noise_fidelity_zero/DC_10_noise.py', 
+    '../regression/scripts/noise_fidelity_one/propeller_noise.py',
+    '../regression/scripts/noise_fidelity_one/aircraft_noise.py',
+    '../regression/scripts/nonuniform_propeller_inflow/nonuniform_propeller_inflow.py',
+    '../regression/scripts/optimization_packages/optimization_packages.py',
+    '../regression/scripts/payload_range/payload_range.py',
+    '../regression/scripts/plots/plot_test.py',
+    '../regression/scripts/propeller/propeller_test.py',
+    '../regression/scripts/propeller_speeds/range_endurance_speeds.py',
+    '../regression/scripts/propulsion_surrogate/propulsion_surrogate.py',
+    '../regression/scripts/ramjet_network/ramjet_network.py',
+    '../regression/scripts/Regional_Jet_Optimization/Optimize2.py',
+    '../regression/scripts/scramjet_network/scramjet_network.py',
+    '../regression/scripts/rocket_network/Rocketdyne_F1.py',
+    '../regression/scripts/rocket_network/Rocketdyne_J2.py',
+    '../regression/scripts/segments/segment_test.py',
+    '../regression/scripts/segments/transition_segment_test.py',
+    '../regression/scripts/slipstream/slipstream_test.py',
+    '../regression/scripts/slipstream/propeller_interactions.py',
+    '../regression/scripts/solar_network/solar_network.py',
+    '../regression/scripts/solar_network/solar_low_fidelity_network.py',
+    '../regression/scripts/solar_radiation/solar_radiation.py',
+    '../regression/scripts/SU2_surrogate/BWB-450.py',
+    '../regression/scripts/sweeps/test_sweeps.py',
+    '../regression/scripts/test_input_output/test_xml_read_write.py',
+    '../regression/scripts/test_input_output/test_freemind_write.py',
+    '../regression/scripts/turboelectric_HTS_ducted_fan_network/turboelectric_HTS_ducted_fan_network.py',
+    '../regression/scripts/turboelectric_HTS_dynamo_ducted_fan_network/turboelectric_HTS_dynamo_ducted_fan_network.py',
+    '../regression/scripts/variable_cruise_distance/variable_cruise_distance.py',
+    '../regression/scripts/V_n_diagram/V_n_diagram_regression.py',
+    '../regression/scripts/VTOL/test_Multicopter.py',
+    '../regression/scripts/VTOL/test_Tiltwing.py',
+    '../regression/scripts/VTOL/test_Stopped_Rotor.py',
+    '../regression/scripts/weights/weights.py'
     
 ]
 
@@ -117,11 +117,11 @@ modules = [
 #   Main
 # ----------------------------------------------------------------------
 
-def main():
+def main(modules_to_test):
 
     # preallocate test results
     results = DataOrdered()
-    for module in modules:
+    for module in modules_to_test:
         results[module] = 'Untested'
 
     sys.stdout.write('# --------------------------------------------------------------------- \n')
@@ -132,7 +132,7 @@ def main():
 
     # run tests
     all_pass = True
-    for module in modules:
+    for module in modules_to_test:
         passed = test_module(module)
         if passed:
             results[module] = '  Passed'
@@ -221,4 +221,4 @@ def test_module(module_path):
 # ----------------------------------------------------------------------
 
 if __name__ == '__main__':
-    main()
+    main(modules)
