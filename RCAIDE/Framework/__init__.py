15813a17b7abc661d148b7246956602d04278465
Jordan <jtrentsmart@gmail.com>
2024-03-14 14:02:21 -0500

    Framework and Library Linking

--- RCAIDE/Framework/__init__.py
+++ RCAIDE/Framework/__init__.py
@@ -15,3 +15,9 @@ from . import External_Interfaces
 from . import Optimization
 from . import Mission
 from . import Networks
+
+from . import Framework_Methods as Methods
+
+from ..Library import Attributes
+from ..Library import Components
+from ..Library import Plots
