9aeb91a80430c71d8bf3d8794837bb0b94b3459a
Matthew Clarke <mclarke2@stanford.edu>
2024-04-24 21:47:54 -0500

    Update __init__.py

--- RCAIDE/Framework/Core/__init__.py
+++ RCAIDE/Framework/Core/__init__.py
@@ -9,14 +9,10 @@
 #  IMPORT
 # ----------------------------------------------------------------------------------------------------------------------
 
-from Legacy.trunk.S.Core           import Data
-from Legacy.trunk.S.Core           import DataOrdered
-from Legacy.trunk.S.Core           import Diffed_Data, diff
-from Legacy.trunk.S.Core           import ContainerOrdered
-from Legacy.trunk.S.Core           import *
-from .Container                    import Container
-from .Utilities                    import interp2d
-from .Utilities                    import orientation_product
-from .Utilities                    import orientation_transpose
-from .Utilities                    import angles_to_dcms
-from Legacy.trunk.S.Core           import Units
+from Legacy.trunk.S.Core.Data          import Data
+from Legacy.trunk.S.Core.DataOrdered   import DataOrdered
+from Legacy.trunk.S.Core.Diffed_Data   import Diffed_Data, diff
+from Legacy.trunk.S.Core               import ContainerOrdered
+from .Container                        import Container
+from .Utilities                        import *
+from Legacy.trunk.S.Core.Units         import Units 
