c1cb1f78b943376a5a05ed765a6d1e133df2b2cf
Matthew Clarke <mclarke2@stanford.edu>
2024-04-24 22:07:04 -0500

    Update __init__.py

--- RCAIDE/Framework/Core/__init__.py
+++ RCAIDE/Framework/Core/__init__.py
@@ -12,8 +12,7 @@
 from Legacy.trunk.S.Core.Data          import Data
 from Legacy.trunk.S.Core.DataOrdered   import DataOrdered
 from Legacy.trunk.S.Core.Diffed_Data   import Diffed_Data, diff
-from Legacy.trunk.S.Core               import ContainerOrdered
+from Legacy.trunk.S.Core               import ContainerOrdered , redirect
 from .Container                        import Container
-from .Utilities                        import *
-from Legacy.trunk.S.Core.redirect      import redirect
+from .Utilities                        import * 
 from Legacy.trunk.S.Core.Units         import Units 
