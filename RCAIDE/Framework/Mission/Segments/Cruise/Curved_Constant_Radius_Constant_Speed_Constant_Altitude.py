f531e1895ccfa26049401445eddf7025eabca14c
ARmolloy <167649645+ARmolloy@users.noreply.github.com>
2024-09-23 17:38:19 -0500

    Move files from curved flight

---
+++ RCAIDE/Framework/Mission/Segments/Cruise/Curved_Constant_Radius_Constant_Speed_Constant_Altitude.py
@@ -0,0 +1,71 @@
+## @ingroup Analyses-Mission-Segments-Cruise 
+# RCAIDE/Framework/Analyses/Mission/Segments/Cruise/Curved_Constant_Radius_Constant_Speed_Constant_Altitude.py
+# 
+# 
+# Created:  September 2024, A. Molloy, M. Clarke
+ 
+# ----------------------------------------------------------------------------------------------------------------------
+#  IMPORT
+# ----------------------------------------------------------------------------------------------------------------------
+
+# RCAIDE imports  
+from RCAIDE.Framework.Mission.Segments.Evaluate   import Evaluate 
+from RCAIDE.Framework.Core                        import Units   
+from RCAIDE.Library.Mission                       import Common,Segments
+
+# ----------------------------------------------------------------------------------------------------------------------
+#  Curved_Constant_Radius_Constant_Speed_Constant_Altitude
+# ----------------------------------------------------------------------------------------------------------------------  
+
+## @ingroup Analyses-Mission-Segments-Cruise
+class Curved_Constant_Radius_Constant_Speed_Constant_Altitude(Evaluate):
+    """ Curved path with fixed true airspeed and altitude and a set sector arc witha  constant radius.
+       
+        Assumptions:
+        None
+        
+        Source:
+        None
+    """         
+    
+    def __defaults__(self):
+        """ This sets the default solver flow. Anything in here can be modified after initializing a segment.
+    
+            Assumptions:
+            None
+    
+            Source:
+            N/A
+    
+            Inputs:
+            None
+    
+            Outputs:
+            None
+    
+            Properties Used:
+            None
+        """           
+        
+        # -------------------------------------------------------------------------------------------------------------- 
+        # User Inputs
+        # -------------------------------------------------------------------------------------------------------------- 
+        self.altitude          = None
+        self.air_speed         = None 
+        self.radius            = None 
+        self.start_true_course = 0.0 * Units.degrees 
+        self.turn_angle        = 0.0 * Units.degrees # + indicated right hand turn, negative indicates left-hand turn defaults to straight flight/won't actually turn? 
+        self.true_course       = 0.0 * Units.degrees  ### look into
+
+        # -------------------------------------------------------------------------------------------------------------- 
+        #  Mission specific processes 
+        # -------------------------------------------------------------------------------------------------------------- 
+        initialize                         = self.process.initialize  
+        initialize.conditions              = Segments.Cruise.Curved_Constant_Radius_Constant_Speed_Constant_Altitude.initialize_conditions  
+        iterate                            = self.process.iterate   
+        iterate.unknowns.mission           = Common.Unpack_Unknowns.orientation
+        iterate.unknowns.controls          = Common.Unpack_Unknowns.control_surfaces
+        iterate.residuals.flight_dynamics  = Common.Residuals.flight_dynamics
+ 
+        return
+
---
+++ RCAIDE/Library/Mission/Segments/Cruise/Curved_Constant_Radius_Constant_Speed_Constant_Altitude.py
@@ -0,0 +1,85 @@
+## @ingroup Library-Missions-Segments-Cruise
+# RCAIDE/Library/Missions/Segments/Cruise/Curved_Constant_Radius_Constant_Speed_Constant_Altitude.py
+# 
+# 
+# Created:  September 2024, A. Molloy + M. Clarke
+
+# ----------------------------------------------------------------------------------------------------------------------
+#  IMPORT
+# ---------------------------------------------------------------------------------------------------------------------- 
+# Package imports 
+import numpy as np
+ 
+# ----------------------------------------------------------------------------------------------------------------------
+#  Initialize Conditions
+# ----------------------------------------------------------------------------------------------------------------------
+## @ingroup Library-Missions-Segments-Cruise
+def initialize_conditions(segment):
+    """Sets the specified conditions which are given for the segment type.
+
+    Assumptions:
+    Curved segment with constant radius, constant speed and constant altitude
+
+    Source:
+    N/A
+
+    Inputs:
+    segment.altitude                [meters] # deleted segment distance as it is now defined by the turn_angle and radius.
+    segment.speed                   [meters/second]
+    self.start_true_course          [degrees] true course of the vehicle before the turn
+    self.turn_angle                 [degrees] angle measure of the curve. + is right hand turn, - is left hand turn. 
+    self.radius                     [meters] radius of the turn
+    
+    Outputs: ***pretty sure that no additional outputs are need. Verify this***
+    conditions.frames.inertial.velocity_vector  [meters/second]
+    conditions.frames.inertial.position_vector  [meters]
+    conditions.freestream.altitude              [meters]
+    conditions.frames.inertial.time             [seconds]
+
+    Properties Used:
+    N/A
+    """        
+    
+    # unpack 
+    alt        = segment.altitude
+    air_speed  = segment.air_speed       
+    beta       = segment.sideslip_angle
+    radius = segment.radius
+    start_true_course = segment.start_true_course
+    arc_sector = segment.turn_angle
+    conditions = segment.state.conditions 
+
+    # check for initial velocity
+    if air_speed is None: 
+        if not segment.state.initials: raise AttributeError('airspeed not set')
+        air_speed = np.linalg.norm(segment.state.initials.conditions.frames.inertial.velocity_vector[-1])
+        
+    # check for initial altitude
+    if alt is None:
+        if not segment.state.initials: raise AttributeError('altitude not set')
+        alt = -1.0 * segment.state.initials.conditions.frames.inertial.position_vector[-1,2]
+    
+    # check for initial altitude
+    if radius is None:
+        if not segment.state.initials: raise AttributeError('radius not set')
+        radius = 0.1 # minimum radius so as to approximate a near instantaneous curve
+    
+    if arc_sector is None:
+        if not segment.state.initials: raise AttributeError('turn angle not set')
+        arc_sector = 0.0 # aircraft does not turn    
+
+    # dimensionalize time
+    v_x         = np.cos(beta + arc_sector + start_true_course)*air_speed # Updated to reflect final heading. Original: np.cos(beta)*air_speed
+    v_y         = np.sin(beta + arc_sector + start_true_course)*air_speed # Updated to refelct final heading. Original: np.sin(beta)*air_speed
+    t_initial   = conditions.frames.inertial.time[0,0]
+    t_final     = (np.abs(arc_sector) *np.pi /180) * radius / air_speed + t_initial # updated
+    t_nondim    = segment.state.numerics.dimensionless.control_points
+    time        = t_nondim * (t_final-t_initial) + t_initial
+    
+    # pack
+    '''Is this at the end of the the segment?'''
+    segment.state.conditions.freestream.altitude[:,0]             = alt
+    segment.state.conditions.frames.inertial.position_vector[:,2] = -alt # z points down
+    segment.state.conditions.frames.inertial.velocity_vector[:,0] = v_x
+    segment.state.conditions.frames.inertial.velocity_vector[:,1] = v_y
+    segment.state.conditions.frames.inertial.time[:,0]            = time[:,0]
