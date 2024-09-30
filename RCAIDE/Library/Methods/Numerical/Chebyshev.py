787e1b8dd313f6c59bd2bc125d517b51f960820c
JTSmart <jtrentsmart@gmail.com>
2024-09-30 08:56:07 -0500

    Updated Default Conditions

---
+++ RCAIDE/Library/Methods/Numerical/Chebyshev.py
@@ -0,0 +1,29 @@
+# RCAIDE/Library/Methods/Numerical/Chebyshev.py
+# (c) Copyright 2024 Aerospace Research Community LLC
+#
+# Created: Sep 2024, RCAIDE Team
+
+# ----------------------------------------------------------------------------------------------------------------------
+#  IMPORT
+# ----------------------------------------------------------------------------------------------------------------------
+
+from dataclasses import dataclass, field
+
+# package imports
+import numpy as np
+
+# RCAIDE imports
+import RCAIDE.Framework as rcf
+
+# ----------------------------------------------------------------------------------------------------------------------
+#  Chebyshev
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def numerical_Chebyshev(State: rcf.State,
+                   Settings: rcf.Settings,
+                   System: rcf.System):
+
+
+                   
+    return State, Settings, System
