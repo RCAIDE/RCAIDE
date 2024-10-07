e567554e10475d6aa85c38c13e81927bb80e02de
JTSmart <jtrentsmart@gmail.com>
2024-10-07 08:33:28 -0500

    Freestream Template

--- RCAIDE/Framework/Missions/Conditions/Freestream.py
+++ RCAIDE/Framework/Missions/Conditions/Freestream.py
@@ -7,6 +7,7 @@
 #  IMPORT
 # ----------------------------------------------------------------------------------------------------------------------
 
+import unittest
 from dataclasses import dataclass, field
 
 # package imports
@@ -28,7 +29,7 @@ class FreestreamConditions(Conditions):
     This class encapsulates various atmospheric and flight parameters that define
     the freestream conditions for aerodynamic analysis or simulation.
 
-    Parameters
+    Attributes
     ----------
     name : str, optional
         Name of the freestream condition. Default is 'Freestream'.
@@ -71,10 +72,6 @@ class FreestreamConditions(Conditions):
     delta_ISA : np.ndarray, optional
         Deviation from International Standard Atmosphere. Default is zeros((1, 1)).
 
-    Attributes
-    ----------
-    Same as parameters.
-
     Notes
     -----
     All attributes are initialized as zero arrays of shape (1, 1) by default.
@@ -104,3 +101,58 @@ class FreestreamConditions(Conditions):
     reynolds_number:    np.ndarray  = field(default_factory=lambda: np.zeros((1, 1)))
 
     delta_ISA:          np.ndarray  = field(default_factory=lambda: np.zeros((1, 1)))
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Unit Tests
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+class TestFreestreamConditions(unittest.TestCase):
+    def setUp(self):
+        self.freestream = FreestreamConditions()
+
+    def test_default_values(self):
+        self.assertEqual(self.freestream.name, 'Freestream')
+        np.testing.assert_array_equal(self.freestream.velocity, np.zeros((1, 1)))
+        np.testing.assert_array_equal(self.freestream.u, np.zeros((1, 1)))
+        np.testing.assert_array_equal(self.freestream.v, np.zeros((1, 1)))
+        np.testing.assert_array_equal(self.freestream.w, np.zeros((1, 1)))
+        np.testing.assert_array_equal(self.freestream.altitude, np.zeros((1, 1)))
+        np.testing.assert_array_equal(self.freestream.gravity, np.zeros((1, 1)))
+        np.testing.assert_array_equal(self.freestream.pressure, np.zeros((1, 1)))
+        np.testing.assert_array_equal(self.freestream.temperature, np.zeros((1, 1)))
+        np.testing.assert_array_equal(self.freestream.density, np.zeros((1, 1)))
+        np.testing.assert_array_equal(self.freestream.speed_of_sound, np.zeros((1, 1)))
+        np.testing.assert_array_equal(self.freestream.dynamic_viscosity, np.zeros((1, 1)))
+        np.testing.assert_array_equal(self.freestream.dynamic_pressure, np.zeros((1, 1)))
+        np.testing.assert_array_equal(self.freestream.mach_number, np.zeros((1, 1)))
+        np.testing.assert_array_equal(self.freestream.reynolds_number, np.zeros((1, 1)))
+        np.testing.assert_array_equal(self.freestream.delta_ISA, np.zeros((1, 1)))
+
+    def test_custom_name(self):
+        custom_freestream = FreestreamConditions(name="Custom Freestream")
+        self.assertEqual(custom_freestream.name, "Custom Freestream")
+
+    def test_custom_values(self):
+        custom_freestream = FreestreamConditions(
+            velocity=np.array([[100.0]]),
+            altitude=np.array([[1000.0]]),
+            temperature=np.array([[288.15]]),
+            pressure=np.array([[101325.0]])
+        )
+        np.testing.assert_array_equal(custom_freestream.velocity, np.array([[100.0]]))
+        np.testing.assert_array_equal(custom_freestream.altitude, np.array([[1000.0]]))
+        np.testing.assert_array_equal(custom_freestream.temperature, np.array([[288.15]]))
+        np.testing.assert_array_equal(custom_freestream.pressure, np.array([[101325.0]]))
+
+    def test_array_shape(self):
+        for attr_name in ['velocity', 'u', 'v', 'w', 'altitude', 'gravity', 'pressure',
+                          'temperature', 'density', 'speed_of_sound', 'dynamic_viscosity',
+                          'dynamic_pressure', 'mach_number', 'reynolds_number', 'delta_ISA']:
+            with self.subTest(attribute=attr_name):
+                attr_value = getattr(self.freestream, attr_name)
+                self.assertEqual(attr_value.shape, (1, 1), f"{attr_name} shape is not (1, 1)")
+
+
+if __name__ == '__main__':
+    unittest.main()
