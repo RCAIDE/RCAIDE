c4444e4a916e63e92dfa846ee29f4df6c01b237c
ARmolloy <167649645+ARmolloy@users.noreply.github.com>
2025-07-01 16:47:34 -0500

    Update compressible_mixed_flat_plate.py
    Updated turbulent skin friction formula to account for skin roughness as given by Shevell's Fundamentals of Flight

--- RCAIDE/Library/Methods/Aerodynamics/Common/Drag/compressible_mixed_flat_plate.py
+++ RCAIDE/Library/Methods/Aerodynamics/Common/Drag/compressible_mixed_flat_plate.py
@@ -48,11 +48,16 @@ def compressible_mixed_flat_plate(Re,Ma,Tc,xt):
     xeff  = (27.78*theta*Re**0.2)**1.25
     Rext  = Re*(1-xt+xeff)
     
-    cf_turb  = 0.455/(np.log10(Rext)**2.58)
+    #cf_turb  = 0.455/(np.log10(Rext)**2.58) # Schlichting empircal formula for turbulent flat plate. note that this correlation is meant for smooth surfaces.
+
+    # Turbulent flat plate correlation from Shevell's Fundamentals of Flight. Accounts for typical roughness of a transport aircraft
+    y = np.array([0.01273, 0.0090077, 0.0058721317, 0.00716019, 0.0049416, 0.003744045, 0.00316321, 0.002506763, 0.002064, 0.0016604122])
+    x = np.array([10000, 115300, 675100, 281300, 1492600, 6040200, 14920400, 58584300, 193593000, 897594100])
+    cf_turb = np.interp(Rext, x, y) # Data is from Figure 11.2 of Shevell's Fundamentals of Flight.
     cf_lam   = 1.328/(Rex**0.5)
     
     if xt > 0.0:
-        cf_start = 0.455/(np.log10(Re*xeff)**2.58)
+        cf_start = np.interp(Re*xeff, x, y) #0.455/(np.log10(Re*xeff)**2.58)
     else:
         cf_start = 0.0
     
