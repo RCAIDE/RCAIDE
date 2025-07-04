e794be516d8dfd41b8cd67a102a5233011015833
Matthew Clarke <mclarke2@stanford.edu>
2025-07-03 18:24:51 -0700

    Update VLM.py

--- RCAIDE/Library/Methods/Aerodynamics/Vortex_Lattice_Method/VLM.py
+++ RCAIDE/Library/Methods/Aerodynamics/Vortex_Lattice_Method/VLM.py
@@ -549,11 +549,7 @@ def VLM(conditions,settings,geometry):
         i+=1
         
     results.CLift_wings         = Clift_wings
-    results.CDrag_induced_wings = Cdrag_wings
-    
-    ## OLD (previous VLM)
-    DRAG                             = CDC*ES 
-    results.CDrag_induced            = np.atleast_2d(np.sum(DRAG,axis=1)/SREF).T 
+    results.CDrag_induced_wings = Cdrag_wings  
     return results
 
 # ----------------------------------------------------------------------
