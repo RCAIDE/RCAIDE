efcba58b7e20ec6e921b2ea920daf05623eaabb4
Matthew Clarke <mclarke2@stanford.edu>
2025-07-21 10:58:50 -0700

    updated VLM

--- RCAIDE/Library/Methods/Aerodynamics/Vortex_Lattice_Method/VLM.py
+++ RCAIDE/Library/Methods/Aerodynamics/Vortex_Lattice_Method/VLM.py
@@ -359,12 +359,12 @@ def VLM_Routine(conditions,settings,geometry, x_m, z_m, S_ref, b_ref,c_bar,delta
     m_unique      = np.atleast_2d(m_unique).T
     inv           = inv.reshape(-1) # this is done to ensure compatibility across numpy1.0 and numpy2.0
     #C_mn_small, s, RFLAG_small, EW_small = compute_wing_induced_velocity(VD,m_unique,compute_EW=True)
-    C_mn_small, s, RFLAG_small, EW_small = compute_wing_induced_velocity(VD,mach,compute_EW=True)
+    #C_mn  = C_mn_small[inv,:,:,:]
+    #RFLAG = RFLAG_small[inv,:]
+    #EW    = EW_small[inv,:,:]
     
-    C_mn  = C_mn_small[inv,:,:,:]
-    RFLAG = RFLAG_small[inv,:]
-    EW    = EW_small[inv,:,:]
-
+    C_mn, s, RFLAG, EW = compute_wing_induced_velocity(VD,mach,compute_EW=True)
+    
     # Turn off sonic vortices when Mach>1
     RHS = RHS*RFLAG
     
