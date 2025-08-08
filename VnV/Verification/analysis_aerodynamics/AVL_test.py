99f429393e0367c789108e12df18a2463e3bcac3
Matthew Clarke <mclarke2@stanford.edu>
2025-08-08 14:42:46 -0700

    Update AVL_test.py

--- VnV/Verification/analysis_aerodynamics/AVL_test.py
+++ VnV/Verification/analysis_aerodynamics/AVL_test.py
@@ -65,7 +65,7 @@ def AVL_Surrogate_Mission(use_surrogate,trim_aircraft,keep_regression_files,new_
  
     # Extract sample values from computation   
     cruise_CL        = results.segments.cruise.conditions.aerodynamics.coefficients.lift.total[2][0] 
-    cruise_CL_thruth = 0.46801229733355454
+    cruise_CL_thruth = 0.4662357296346775
     # Truth values  
     error = Data()  
     error.cruise_CL   = np.max(np.abs(cruise_CL - cruise_CL_thruth))   
