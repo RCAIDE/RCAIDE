803d67d48907984d6b6e24dcf8da70edf1ee0bd6
Matteo Guidotti <matteog2@illinois.edu>
2025-08-08 16:24:36 -0500

    Fix PR

--- VnV/Verification/analysis_aerodynamics/blended_wing_body_aerodynamics_test.py
+++ VnV/Verification/analysis_aerodynamics/blended_wing_body_aerodynamics_test.py
@@ -45,7 +45,7 @@ def main():
                     show_figure=False)
 
     Cruise_CL        = results.segments.cruise.conditions.aerodynamics.coefficients.lift.total[2][0] 
-    Cruise_CL_true   = 0.38465110612833114
+    Cruise_CL_true   = 0.38413007873149724
     Cruise_CL_diff   = np.abs(Cruise_CL - Cruise_CL_true)
     print('Error: ',Cruise_CL_diff)
     assert np.abs((Cruise_CL - Cruise_CL_true)/Cruise_CL_true) < 1e-6
