3e9f3576f4accfea8ee3d14923e3ca2223dd22ea
Sai Sankalp Shekar <sshekar2@illinois.edu>
2025-09-02 21:24:30 -0500

    delete SU2

--- RCAIDE/Framework/External_Interfaces/OpenVSP/__init__.py
+++ RCAIDE/Framework/External_Interfaces/OpenVSP/__init__.py
@@ -19,4 +19,3 @@ from .vsp_nacelle              import *
 from .vsp_wing                 import *
 from .vsp_rotor                import *
 from .write_vsp_mesh           import write_vsp_mesh
-from .run_vsp_mesh 	       import run_vsp_mesh
