9f5af24af5a94e2f97ef1d51940cbaa19c72df57
Madison V. Carter <mvc7@illinois.edu>
2025-09-18 10:57:02 -0500

    Update fuselage_planform.py
    Truncated Elliptic Cone segment equation for wetted area implemented.

--- RCAIDE/Library/Methods/Geometry/Planform/fuselage_planform.py
+++ RCAIDE/Library/Methods/Geometry/Planform/fuselage_planform.py
@@ -60,13 +60,19 @@ def fuselage_planform(fuselage, circular_cross_section = True):
     b     = fuselage_height/2. 
     R     = (a-b)/(a+b)
     
-    
+    # a =      base semi-major axis  
+    # b =      base semi-minor axis  
+    # c =      top semi-major axis  
+    # d =      top semi-minor axis  
+    # h =      height of segment
+    # h=       fuselage.length * (seg_2.percent_x_location -  seg_1.percent_x_location)
+
 
     side_projected_area  = 0 
     wetted_area          = 0   
     front_projected_area = 0 
     effective_diameter   = 0 
-        
+    
     if len(fuselage.segments) > 2:
         f_segs = list(fuselage.segments.keys())
         for i in range(len(fuselage.segments)-1):
@@ -74,14 +80,16 @@ def fuselage_planform(fuselage, circular_cross_section = True):
             seg_1 = fuselage.segments[f_segs[i]]
             seg_2 = fuselage.segments[f_segs[i+1]]
               
-            delta_x                = fuselage.length * (seg_2.percent_x_location -  seg_1.percent_x_location)
+            delta_x                = fuselage.lengths.total * (seg_2.percent_x_location -  seg_1.percent_x_location)
             side_projected_area   += ((seg_1.height +  seg_2.height ) / 2) * delta_x 
-            wetted_area           += MADISON ADD CODE HERE 
-            
-            A_1  = np.pi *  (seg_1.height / 2) *  (seg_1.weight / 2)   
-            A_2  = np.pi *  (seg_2.height / 2) *  (seg_2.weight / 2)    
+
+            area = truncated_elliptic_cone_lateral_area(seg_1.width/2, seg_1.height/2, seg_2.width/2, seg_2.height/2, delta_x)
+            wetted_area += area
+
+            A_1  = np.pi *  (seg_1.height / 2) *  (seg_1.width / 2)   
+            A_2  = np.pi *  (seg_2.height / 2) *  (seg_2.width / 2)    
             front_projected_area  = np.maximum(front_projected_area,np.maximum(A_1,A_2)  )
-        effective_diameter    +=  
+        
 
     else:    
         side_projected_area  = fuselage.heights.maximum * fuselage.lengths.total  
@@ -89,7 +97,7 @@ def fuselage_planform(fuselage, circular_cross_section = True):
                                np.pi*a*(a+ np.sqrt( fuselage.lengths.tail**2 +(a)**2))+ \
                                np.pi * fuselage.width * ( fuselage.lengths.total - (fuselage.lengths.tail+ fuselage.lengths.nose))  
         front_projected_area = np.pi * a *  b
-        effective_diameter   = ((fuselage_width/2)+(fuselage_height/2.))*(64.-3.*R**4)/(64.-16.*R**2) 
+    effective_diameter   = ((fuselage_width/2)+(fuselage_height/2.))*(64.-3.*R**4)/(64.-16.*R**2) 
     
 
     fuselage.lengths.nose          = nose_length
@@ -101,3 +109,10 @@ def fuselage_planform(fuselage, circular_cross_section = True):
     fuselage.effective_diameter    = effective_diameter 
 
     return 
+
+def truncated_elliptic_cone_lateral_area(a, b, c, d, h):
+    
+    s_major = np.sqrt(h**2 + (a - c)**2)  # slant length in major axis direction
+    s_minor = np.sqrt(h**2 + (b - d)**2)  # slant length in minor axis direction
+    wetted_area = np.pi * ((a + c)/2) * s_major + np.pi * ((b + d)/2) * s_minor
+    return wetted_area
