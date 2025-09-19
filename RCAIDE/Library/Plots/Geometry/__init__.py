2fae08134a10fad24e58f7705663a71307a693bb
Matthew Clarke <mclarke2@stanford.edu>
2025-09-19 18:58:33 -0500

    Update __init__.py

--- RCAIDE/Library/Plots/Geometry/__init__.py
+++ RCAIDE/Library/Plots/Geometry/__init__.py
@@ -30,6 +30,9 @@ from .plot_3d_nacelle                         import generate_3d_stack_nacelle_p
 from .plot_3d_fuel_tank                       import plot_3d_non_integral_fuel_tank
 from .plot_3d_fuel_tank                       import plot_3d_integral_fuselage_tank
 from .plot_3d_fuel_tank                       import plot_3d_integral_wing_tank
+from .plot_3d_fuel_tank                       import generate_non_integral_fuel_tank_points
+from .plot_3d_fuel_tank                       import generate_integral_fuel_tank_points
+from .plot_3d_fuel_tank                       import generate_integral_wing_tank_points
 from .plot_3d_wing                            import plot_3d_wing
 from .plot_3d_fuselage                        import plot_3d_fuselage
 from .plot_3d_fuselage                        import generate_3d_fuselage_points
