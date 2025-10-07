fe7314d0fe703d203a2cd0dfdb41fc748a14076e
Sai Sankalp Shekar <sshekar2@illinois.edu>
2025-10-07 14:31:43 -0500

    Fix comments and misc weight calculation errors
    Corrected comments in Aerodynamics.py to refer to drag reduction instead of weight reduction. Fixed the misc operating items weight calculation in compute_operating_items.py by removing double-counting of WSRV. Reordered function calls in geometry.py for Blended Wing Body to ensure planform properties are computed before passenger accommodations.

--- RCAIDE/Library/Methods/Mass_Properties/Weight_Buildups/Conventional/BWB/FLOPS/compute_operating_items.py
+++ RCAIDE/Library/Methods/Mass_Properties/Weight_Buildups/Conventional/BWB/FLOPS/compute_operating_items.py
@@ -107,7 +107,7 @@ def compute_operating_items_weight(vehicle):
     WSRV = (5.164*NPF + 3.846*NPB + 2.529*NPE)*(DESRNG/VMAX)**0.225
     
     output                           = Data()
-    output.misc                      = WUF * Units.lbs + WOIL * Units.lbs + WSRV * Units.lbs + WCON * Units.lbs
+    output.misc                      = WUF * Units.lbs + WOIL * Units.lbs + WCON * Units.lbs
     output.flight_crew               = WFLCRB * Units.lbs
     output.flight_attendants         = WFLAAB * Units.lbs
     output.passenger_service         = WSRV   * Units.lbs
