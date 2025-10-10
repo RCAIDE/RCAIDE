6d3806919364c24aa401f22b566ba44e2f6634ed
ARmolloy <167649645+ARmolloy@users.noreply.github.com>
2025-10-10 12:55:53 -0500

    Regression Electric Transport

--- VnV/Verification/analysis_weights/operating_empty_weight_test.py
+++ VnV/Verification/analysis_weights/operating_empty_weight_test.py
@@ -1,6 +1,6 @@
 # weights.py
 import  RCAIDE
-from RCAIDE.Framework.Analyses.Weights import Electric_General_Aviation
+from RCAIDE.Framework.Analyses.Weights import Electric_General_Aviation, Electric_Transport
 from RCAIDE.Framework.Core import Data, Units 
 from RCAIDE.Library.Methods.Powertrain.Propulsors.Turbofan   import design_turbofan  
 from RCAIDE.Library.Plots import * 
@@ -21,18 +21,52 @@ from BWB                    import vehicle_setup as bwb_setup
 from Stopped_Rotor_EVTOL    import vehicle_setup as evtol_setup
 from Boeing_787             import vehicle_setup as hydrogen_transport_setup
 from Electric_Twin_Otter    import vehicle_setup as electric_general_aviation_setup
+from all_electric_ATR_72    import vehicle_setup as electric_transport_setup
 
 def main():
     update_regression_values = False # should be false unless code functionally changes
     show_figure              = False # leave false for regression
 
-    Transport_Aircraft_Test(update_regression_values,show_figure)
-    BWB_Aircraft_Test(update_regression_values,show_figure)
-    General_Aviation_Test(update_regression_values,show_figure)
-    EVTOL_Aircraft_Test(update_regression_values,show_figure)
-    Transport_Hydrogen_Test(update_regression_values,show_figure)
-    Electric_General_Aviation_Test(update_regression_values,show_figure)
+    # Transport_Aircraft_Test(update_regression_values,show_figure)
+    # BWB_Aircraft_Test(update_regression_values,show_figure)
+    # General_Aviation_Test(update_regression_values,show_figure)
+    # EVTOL_Aircraft_Test(update_regression_values,show_figure)
+    # Transport_Hydrogen_Test(update_regression_values,show_figure)
+    # Electric_General_Aviation_Test(update_regression_values,show_figure)
+    Electric_Transport_Test(update_regression_values,show_figure)
     return
+
+def Electric_Transport_Test(update_regression_values, show_figure):
+    method_types = ['Semi_Empirical']
+
+    vehicle = electric_transport_setup()
+    vehicle.mass_properties.takeoff = None
+    for method_type in method_types:
+        print(f'Testing Transport Aircraft Method: {method_type} | Method: {"Complex"}')        
+        weight_analysis = RCAIDE.Framework.Analyses.Weights.Electric_Transport()
+        weight_analysis.vehicle = vehicle
+        weight = weight_analysis.evaluate()
+        save_path = os.path.join(os.path.dirname(__file__), f'electric_transport_{method_type}.res')
+
+        if update_regression_values:
+            save_results(weight, save_path)
+        old_weight = load_results(save_path)
+
+        check_list = [
+            'payload.total', 'payload.baggage',
+            'empty.structural.wings', 'empty.structural.fuselage',
+            'empty.propulsion.total', 'empty.structural.landing_gear',
+            'empty.systems.total', 'empty.total'
+        ]
+
+        for k in check_list:
+            old_val = old_weight.deep_get(k)
+            new_val = weight.deep_get(k)
+            err = (new_val - old_val) / old_val
+            print(f'{k} Error: {err:.6e}')
+            assert np.abs(err) < 1e-6, f'Check Failed: {k}'
+        print('')
+
 
 def Electric_General_Aviation_Test(update_regression_values, show_figure):
     method_types = ['Physics_Based']
