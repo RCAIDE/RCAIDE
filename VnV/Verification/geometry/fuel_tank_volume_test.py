fc6cf60d582f4c602e3e9e99ccbfa6352fd36371
Sai Sankalp Shekar <sshekar2@illinois.edu>
2025-11-05 14:55:48 -0600

    Update fuel_tank_volume_test.py

--- VnV/Verification/geometry/fuel_tank_volume_test.py
+++ VnV/Verification/geometry/fuel_tank_volume_test.py
@@ -45,7 +45,7 @@ def main():
 
 def single_wing_segment_integral_fuel_tank_volume_test():
 
-    fuel_volume_true = [0.12996673613905557]
+    fuel_volume_true = [0.1604406939938338]
 
     vehicle = Nav_vehicle_setup()
     
