755c614727b57a0204fdfb6de00067d904e3a7ca
Sai Sankalp Shekar <sshekar2@illinois.edu>
2025-12-02 15:47:43 -0600

    Reapply "Rename and refactor operating items weight module"
    This reverts commit 3b9c4f3725fb5c5e2ba26d4d431319a6d893471e.

--- RCAIDE/Library/Methods/Mass_Properties/Weight_Buildups/Conventional/BWB/FLOPS/compute_operating_empty_weight.py
+++ RCAIDE/Library/Methods/Mass_Properties/Weight_Buildups/Conventional/BWB/FLOPS/compute_operating_empty_weight.py
@@ -6,15 +6,15 @@
 #  Imports
 # ----------------------------------------------------------------------------------------------------------------------
 import  RCAIDE
-from RCAIDE.Framework.Core import Data , Units 
-from .compute_aft_center_body_weight import compute_aft_center_body_weight
-from .compute_cabin_weight import compute_cabin_weight
-from .compute_systems_weight import compute_systems_weight
-from .compute_bwb_wing_weight import compute_wing_weight
-from .compute_operating_items import compute_operating_items_weight
-from RCAIDE.Library.Methods.Mass_Properties.Weight_Buildups.Conventional.Common import compute_payload_weight
+from RCAIDE.Framework.Core                                                         import Data , Units 
+from .compute_aft_center_body_weight                                               import compute_aft_center_body_weight
+from .compute_cabin_weight                                                         import compute_cabin_weight
+from .compute_systems_weight                                                       import compute_systems_weight
+from .compute_bwb_wing_weight                                                      import compute_wing_weight
+from .compute_operating_items_weight                                               import compute_operating_items_weight
+from RCAIDE.Library.Methods.Mass_Properties.Weight_Buildups.Conventional.Common    import compute_payload_weight
 from RCAIDE.Library.Methods.Mass_Properties.Weight_Buildups.Conventional.Transport import FLOPS 
-from RCAIDE.Library.Methods.Geometry.Planform                          import segment_properties  
+from RCAIDE.Library.Methods.Geometry.Planform                                      import segment_properties  
  
 from copy import deepcopy
 import numpy as np
