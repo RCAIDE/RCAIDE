9437903d1e4c4330705d932941619c33e7f2ba23
ARmolloy <167649645+ARmolloy@users.noreply.github.com>
2025-12-03 23:18:01 -0600

    Update operating_empty_weight_test.py
    Adding Hydrogen BWB

--- VnV/Verification/analysis_weights/operating_empty_weight_test.py
+++ VnV/Verification/analysis_weights/operating_empty_weight_test.py
@@ -24,7 +24,7 @@ from Electric_Twin_Otter    import vehicle_setup as electric_general_aviation_se
 from all_electric_ATR_72    import vehicle_setup as electric_transport_setup 
 
 def main():
-    update_regression_values = False # should be false unless code functionally changes
+    update_regression_values = True # should be false unless code functionally changes
     show_figure              = False # leave false for regression
 
     Transport_Aircraft_Test(update_regression_values,show_figure)
@@ -32,6 +32,7 @@ def main():
     General_Aviation_Test(update_regression_values,show_figure)
     EVTOL_Aircraft_Test(update_regression_values,show_figure)
     Transport_Hydrogen_Test(update_regression_values,show_figure)
+    BWB_Hydrogen_Aircraft_Test(update_regression_values,show_figure)
     Electric_General_Aviation_Test(update_regression_values,show_figure)
     Electric_Transport_Test(update_regression_values,show_figure)
     return
@@ -291,6 +292,62 @@ def General_Aviation_Test(update_regression_values, show_figure):
             print(f'{k} Error: {err:.6e}')
             assert np.abs(err) < 1e-6, f'Check Failed: {k}'
         print('')
+
+def BWB_Hydrogen_Aircraft_Test(update_regression_values,show_figure):
+    cabin_types = ['Non-PERSUS','PERSUS']
+    for cabin_type in cabin_types:
+        for FLOPS_number in [0,1]:
+            print(f'Testing Hydrogen Transport Aircraft Method: BWB| Composites: {cabin_type} | Method: {"Simple" if FLOPS_number == 0 else "Complex"}') 
+            weight_analysis          = RCAIDE.Framework.Analyses.Weights.Hydrogen_BWB()
+            vehicle  = bwb_setup()
+            for propulsor in vehicle.networks.fuel.propulsors:
+                propulsor.combustor.fuel_data =  RCAIDE.Library.Attributes.Propellants.Liquid_Hydrogen() 
+            for fuel_line in vehicle.networks.fuel.fuel_lines:
+                for fuel_tank in fuel_line.fuel_tanks:
+                    fuel_tank.fuel                                   = RCAIDE.Library.Attributes.Propellants.Liquid_Hydrogen()   
+                    fuel_tank.fuel.gravimetric_efficiency            = 0.5
+                    fuel_tank.material                               = RCAIDE.Library.Attributes.Materials.Aluminum_2219()
+                    fuel_tank.insulation_material                    = RCAIDE.Library.Attributes.Materials.Vacuum_Cellular_Multilayer_Insulation()
+                    fuel_tank.segments_bounding_tank                = ['fuel_wall', 'wing_section_1']
+
+            if cabin_type == 'PERSUS':
+                weight_analysis.settings.PRSEUS = True
+            elif cabin_type == 'Non-PERSUS':
+                weight_analysis.settings.PRSEUS = False
+            for wing in vehicle.wings: 
+                if isinstance(wing, RCAIDE.Library.Components.Wings.Blended_Wing_Body):
+                    compute_layout_of_passenger_accommodations(wing)
+                    bwb_wing_planform(wing)
+                    vehicle.reference_area = wing.areas.reference 
+            weight_analysis.settings.FLOPS.fidelity   = 'Simple' if FLOPS_number == 0 else 'Complex'
+            weight                   = weight_analysis.evaluate(vehicle)
+            plot_weight_breakdown(vehicle, show_figure = show_figure) 
+
+            if update_regression_values:
+                save_results(weight, os.path.join(os.path.dirname(__file__), f"{cabin_type}_{'Simple' if FLOPS_number == 0 else 'Complex'}_weights_Hydrogen_BWB.res"))
+            old_weight = load_results(os.path.join(os.path.dirname(__file__), f"{cabin_type}_{'Simple' if FLOPS_number == 0 else 'Complex'}_weights_Hydrogen_BWB.res"))
+
+            check_list = [
+                'empty.total',
+                'empty.structural.wings', 
+                'empty.structural.total',
+                'empty.propulsion.total',   
+                'empty.systems.total',  
+            ]
+
+            # do the check
+            for k in check_list:
+                print(k)
+
+                old_val = old_weight.deep_get(k)
+                new_val = weight.deep_get(k)
+                err = (new_val-old_val)/old_val
+                print('Error:' , err)
+                assert np.abs(err) < 1e-6 , 'Check Failed : %s' % k     
+
+                print('')
+
+    return
 
 def BWB_Aircraft_Test(update_regression_values,show_figure):
     cabin_types = ['Non-PERSUS','PERSUS']
