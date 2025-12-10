9c3541dfab87042a54735834ce584c629b35d8f4
Matthew Clarke <mclarke2@stanford.edu>
2025-12-10 01:04:53 -0600

    Refactor passenger layout and fuselage geometry routines
    Improved handling of passenger accommodation layout by refactoring logic in compute_layout_of_passenger_accommodations and extracting property computation to a new function. Removed unused cabin_offset attributes and streamlined geometry_preprocess_routine to always compute layouts and planforms. Minor formatting and import corrections applied for consistency.

--- RCAIDE/Library/Methods/Geometry/LOPA/compute_layout_of_passenger_accommodations.py
+++ RCAIDE/Library/Methods/Geometry/LOPA/compute_layout_of_passenger_accommodations.py
@@ -19,88 +19,99 @@ def compute_layout_of_passenger_accommodations(fuselage):
     '''
     Creates the layout of passenger accommodations for a vehicle
     '''
-    #  [ x, y, z , length, width, first-cl flag, business-cl flag, economy-cl flag, seat, emergency-row flag, galley/lav flag, type-A exit flag]
+
     LOPA = np.empty(( 0, 14))
-    side_cabin_offset = 0
-    for cabin in fuselage.cabins:
-        cabin_number_of_seats = 0
-        cabin_class_origin  = [0, 0, 0]
-        total_cabin_length = 0
-        for cabin_class in cabin.classes:
-            seat_data ,cabin_class_origin,cabin_number_of_seats,total_cabin_length  = create_class_seating_map_layout(cabin, cabin_class,cabin_class_origin, side_cabin_offset,cabin_number_of_seats,total_cabin_length)
-            side_cabin_offset = cabin.width / 2
-            LOPA = np.vstack((LOPA,seat_data))
-        cabin.length = total_cabin_length 
-        cabin.number_of_seats = cabin_number_of_seats
     offset_x_overall = 0
-    for cabin in fuselage.cabins:
-        for cabin_class in cabin.classes:
-            cabin_class.percentage = cabin_class.length/cabin.length
-        if not isinstance(cabin,RCAIDE.Library.Components.Fuselages.Cabins.Side_Cabin):
-            offset_x_overall = cabin.origin[0][0]
-    # add cabin offset to account (useful for BWBs and aircraft with H2 tanks)
-    LOPA[:, 2] += fuselage.cabin_offset
+    
+    if len(fuselage.cabins) > 0: 
+        # instantiate dimension of LOPA container 
+        #  [ x, y, z , length, width, first-cl flag, business-cl flag, economy-cl flag, seat, emergency-row flag, galley/lav flag, type-A exit flag]
+        
+        side_cabin_offset = 0
+        for cabin in fuselage.cabins:
+            cabin_number_of_seats = 0
+            cabin_class_origin  = [0, 0, 0]
+            total_cabin_length = 0
+            for cabin_class in cabin.classes:
+                seat_data ,cabin_class_origin,cabin_number_of_seats,total_cabin_length  = create_class_seating_map_layout(cabin, cabin_class,cabin_class_origin, side_cabin_offset,cabin_number_of_seats,total_cabin_length)
+                side_cabin_offset = cabin.width / 2
+                LOPA = np.vstack((LOPA,seat_data))
+            cabin.length = total_cabin_length 
+            cabin.number_of_seats = cabin_number_of_seats
+        
+        # determine offset of LOPA from reference point on aircraft (nose)
+        for cabin in fuselage.cabins:
+            for cabin_class in cabin.classes:
+                cabin_class.percentage = cabin_class.length/cabin.length
+            if not isinstance(cabin,RCAIDE.Library.Components.Fuselages.Cabins.Side_Cabin):
+                offset_x_overall = cabin.origin[0][0]
+                 
+        fuselage.number_of_seats  = np.sum(LOPA[:,10])
+
     fuselage.layout_of_passenger_accommodations                     = Data()
-    fuselage.layout_of_passenger_accommodations.object_coordinates  = LOPA
-    fuselage.number_of_seats                                        = np.sum(LOPA[:,10])
+    fuselage.layout_of_passenger_accommodations.object_coordinates  = LOPA        
     fuselage.layout_of_passenger_accommodations.cabin_x_offset      = offset_x_overall
+
     if LOPA.size > 0 :
-        # Step 1: plot cabin bounds
-        # get points at x min
-        x_min_locs   =  np.where( LOPA[:,2] == min(LOPA[:,2]))[0]
-        x_min        =  LOPA[x_min_locs[0],2] -  LOPA[x_min_locs[0],5]/2
-        x_min_y_max  =  max(LOPA[x_min_locs,3] + LOPA[x_min_locs,6]/2 )
-        x_min_y_min  =  min(LOPA[x_min_locs,3] - LOPA[x_min_locs,6]/2 )
-        x_border_pts = [x_min, x_min]
-        y_border_pts = [x_min_y_min, x_min_y_max]
-        # get points at y max
-        y_max_locs   =  np.where( LOPA[:,3] == max(LOPA[:,3]))[0]
-        y_max        =  LOPA[y_max_locs[0],3] + LOPA[y_max_locs[0],6]/2
-        y_max_x_max  =  max(LOPA[y_max_locs,2] + LOPA[y_max_locs[0],5]/2)
-        y_max_x_min  =  min(LOPA[y_max_locs,2] - LOPA[y_max_locs[0],5]/2)
-        x_border_pts.append(y_max_x_min)
-        x_border_pts.append(y_max_x_max)
-        y_border_pts.append(y_max)
-        y_border_pts.append(y_max)
-        # get points at x max
-        x_max_locs   =  np.where( LOPA[:,2] == max(LOPA[:,2]))[0]
-        x_max        =  LOPA[x_max_locs[0],2] + LOPA[x_max_locs[0],5]/2
-        x_max_y_max  =  max(LOPA[x_max_locs,3] + LOPA[x_max_locs,6]/2)
-        x_max_y_min  =  min(LOPA[x_max_locs,3] - LOPA[x_max_locs,6]/2)
-        x_border_pts.append(x_max)
-        x_border_pts.append(x_max)
-        y_border_pts.append(x_max_y_max)
-        y_border_pts.append(x_max_y_min)
-        # get points at y min
-        y_min_locs   =  np.where( LOPA[:,3] == min(LOPA[:,3]))[0]
-        y_min        =  LOPA[y_min_locs[0],3] - LOPA[y_min_locs[0],6]/2
-        y_min_x_max  =  max(LOPA[y_min_locs,2] + LOPA[y_min_locs[0],5]/2)
-        y_min_x_min  =  min(LOPA[y_min_locs,2] - LOPA[y_min_locs[0],5]/2)
-        x_border_pts.append(y_min_x_max)
-        x_border_pts.append(y_min_x_min)
-        y_border_pts.append(y_min)
-        y_border_pts.append(y_min)
-        # loop through points and determine if there are duplicates
-        y_border_pts = np.array(y_border_pts)
-        x_border_pts = np.array(x_border_pts)
-        # cut where y is negative
-        port_idxs  =  np.where(y_border_pts<0)[0]
-        starboard_x_points = np.delete(x_border_pts, port_idxs)
-        starboard_y_points = np.delete(y_border_pts, port_idxs)
-        fuselage.layout_of_passenger_accommodations.cabin_area_coordinates = np.vstack((starboard_x_points[None,:],starboard_y_points[None, :])).T
-        fuselage.layout_of_passenger_accommodations.cabin_length = max(starboard_x_points)
-        fuselage.layout_of_passenger_accommodations.cabin_width = 2*max(starboard_y_points)
-        if fuselage.width == 0:
-            fuselage.width = fuselage.layout_of_passenger_accommodations.cabin_width
-        # if fuselage.height == 0:
-        #     fuselage.height = fuselage.layout_of_passenger_accommodations.cabin_height
-        if fuselage.lengths.nose == 0:
-            fuselage.lengths.nose          = fuselage.fineness.nose*fuselage.layout_of_passenger_accommodations.cabin_width
-        if fuselage.lengths.tail == 0:
-            fuselage.lengths.tail          = fuselage.fineness.tail*fuselage.layout_of_passenger_accommodations.cabin_width
-        if fuselage.lengths.total == 0:
-            fuselage.lengths.total         = fuselage.lengths.nose + fuselage.lengths.tail + fuselage.layout_of_passenger_accommodations.cabin_length
+        compute_lopa_properties(fuselage, LOPA)    
+        
+    return 
+ 
+def compute_lopa_properties(fuselage, LOPA): 
+    # Step 1: plot cabin bounds
+    # get points at x min
+    x_min_locs   =  np.where( LOPA[:,2] == min(LOPA[:,2]))[0]
+    x_min        =  LOPA[x_min_locs[0],2] -  LOPA[x_min_locs[0],5]/2
+    x_min_y_max  =  max(LOPA[x_min_locs,3] + LOPA[x_min_locs,6]/2 )
+    x_min_y_min  =  min(LOPA[x_min_locs,3] - LOPA[x_min_locs,6]/2 )
+    x_border_pts = [x_min, x_min]
+    y_border_pts = [x_min_y_min, x_min_y_max]
+    
+    # get points at y max
+    y_max_locs   =  np.where( LOPA[:,3] == max(LOPA[:,3]))[0]
+    y_max        =  LOPA[y_max_locs[0],3] + LOPA[y_max_locs[0],6]/2
+    y_max_x_max  =  max(LOPA[y_max_locs,2] + LOPA[y_max_locs[0],5]/2)
+    y_max_x_min  =  min(LOPA[y_max_locs,2] - LOPA[y_max_locs[0],5]/2)
+    x_border_pts.append(y_max_x_min)
+    x_border_pts.append(y_max_x_max)
+    y_border_pts.append(y_max)
+    y_border_pts.append(y_max)
+    
+    # get points at x max
+    x_max_locs   =  np.where( LOPA[:,2] == max(LOPA[:,2]))[0]
+    x_max        =  LOPA[x_max_locs[0],2] + LOPA[x_max_locs[0],5]/2
+    x_max_y_max  =  max(LOPA[x_max_locs,3] + LOPA[x_max_locs,6]/2)
+    x_max_y_min  =  min(LOPA[x_max_locs,3] - LOPA[x_max_locs,6]/2)
+    x_border_pts.append(x_max)
+    x_border_pts.append(x_max)
+    y_border_pts.append(x_max_y_max)
+    y_border_pts.append(x_max_y_min)
+    
+    # get points at y min
+    y_min_locs   =  np.where( LOPA[:,3] == min(LOPA[:,3]))[0]
+    y_min        =  LOPA[y_min_locs[0],3] - LOPA[y_min_locs[0],6]/2
+    y_min_x_max  =  max(LOPA[y_min_locs,2] + LOPA[y_min_locs[0],5]/2)
+    y_min_x_min  =  min(LOPA[y_min_locs,2] - LOPA[y_min_locs[0],5]/2)
+    x_border_pts.append(y_min_x_max)
+    x_border_pts.append(y_min_x_min)
+    y_border_pts.append(y_min)
+    y_border_pts.append(y_min)
+    
+    # loop through points and determine if there are duplicates
+    y_border_pts = np.array(y_border_pts)
+    x_border_pts = np.array(x_border_pts)
+    
+    # cut where y is negative
+    port_idxs  =  np.where(y_border_pts<0)[0]
+    starboard_x_points = np.delete(x_border_pts, port_idxs)
+    starboard_y_points = np.delete(y_border_pts, port_idxs)
+    
+    fuselage.layout_of_passenger_accommodations.cabin_area_coordinates = np.vstack((starboard_x_points[None,:],starboard_y_points[None, :])).T
+    fuselage.layout_of_passenger_accommodations.cabin_length           = max(starboard_x_points)
+    fuselage.layout_of_passenger_accommodations.cabin_width            = 2*max(starboard_y_points)
+    
     return
+
 def create_class_seating_map_layout(cabin,cabin_class,cabin_class_origin, side_cabin_offset,cabin_number_of_seats,cabin_length):
     s_y_coord, cabin_class_origin = get_seat_y_coords(cabin, cabin_class,cabin_class_origin)
     s_x_coord,object_type, cabin_class_origin,cabin_length = get_seat_x_coords(cabin, cabin_class,cabin_class_origin,cabin_length)
