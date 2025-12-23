dc65319b866ad62484bbd37e116227047b55aee7
ARmolloy <167649645+ARmolloy@users.noreply.github.com>
2025-12-23 14:37:42 -0500

    Update Tutorials and NP default setting
    NP does not need to be run for every single analysis so I've changed the default to False. This will reduce computational time

--- RCAIDE/Framework/Analyses/Stability/Stability.py
+++ RCAIDE/Framework/Analyses/Stability/Stability.py
@@ -65,7 +65,7 @@ class Stability(Analysis):
         self.settings.recalculate_total_wetted_area                      = False
         self.settings.oswald_efficiency_factor                           = None
         self.settings.span_efficiency                                    = None
-        self.settings.compute_neutral_point                              = True
+        self.settings.compute_neutral_point                              = False
 
         self.settings.supersonic                                         = Data() 
         self.settings.supersonic.begin_drag_rise_mach_number             = 0.95
