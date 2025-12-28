6003c4387e5aed0de0e20bf750a4d5bb31566079
Matthew Clarke <mclarke2@stanford.edu>
2025-12-28 09:52:08 -0800

    minar changes to wing planform

--- RCAIDE/Library/Methods/Mass_Properties/Weight_Buildups/Hydrogen/BWB/Semi_Empirical/__init__.py
+++ RCAIDE/Library/Methods/Mass_Properties/Weight_Buildups/Hydrogen/BWB/Semi_Empirical/__init__.py
@@ -7,5 +7,5 @@
 # ----------------------------------------------------------------------------------------------------------------------
 #  IMPORT
 # ----------------------------------------------------------------------------------------------------------------------
-from .compute_operating_empty_weight   import compute_operating_empty_weight
+from .compute_operating_empty_weight    import compute_operating_empty_weight
 from .compute_propulsion_system_weight import compute_propulsion_system_weight
