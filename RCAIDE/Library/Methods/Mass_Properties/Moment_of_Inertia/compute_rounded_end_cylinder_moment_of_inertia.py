7bea6f8b6c080c5d7de7e1168bba1a23cc5d87f5
Matthew Clarke <mclarke2@stanford.edu>
2026-01-08 09:49:03 +0900

    Add orientation logic for Non_Integral_Tank MOI calculation
    Introduced logic in compute_rounded_end_cylinder_moment_of_inertia to set the moment of inertia tensor based on the orientation and type of the component, specifically handling Non_Integral_Tank and its bwb_aft_tank property. Also made minor code cleanup in Non_Integral_Tank.py.

--- RCAIDE/Library/Methods/Mass_Properties/Moment_of_Inertia/compute_rounded_end_cylinder_moment_of_inertia.py
+++ RCAIDE/Library/Methods/Mass_Properties/Moment_of_Inertia/compute_rounded_end_cylinder_moment_of_inertia.py
@@ -5,6 +5,8 @@
 # ----------------------------------------------------------------------------------------------------------------------
 #  IMPORT
 # ----------------------------------------------------------------------------------------------------------------------
+# rcaide imports
+import RCAIDE
 
 # package imports 
 import numpy as np 
@@ -100,7 +102,7 @@ def compute_rounded_end_cylinder_moment_of_inertia(component,outer_length,outer_
     mass_sph = mass * (volume_sph / volume  )
         
     # MOI about cylindrical axis
-    d = outer_length / 2 + (3/8)*outer_radius
+    d = outer_length / 2 + (3/8)*outer_radius # distance from centroid to base of hemisphere
     I_cylinder_cylin_axis      = mass_cyl *  ( (outer_radius**2 + inner_radius**2)/4 + (outer_length**2)/12 )
     I_sperical_caps_cylin_axis = 2/5 * 2*mass_sph  *  (outer_radius**5 - inner_radius**5)/ (outer_radius**3 - inner_radius**3)
     I_tot_cylin_axis           = I_cylinder_cylin_axis + I_sperical_caps_cylin_axis + 2*mass_sph*(d**2)
@@ -110,15 +112,21 @@ def compute_rounded_end_cylinder_moment_of_inertia(component,outer_length,outer_
     I_sperical_caps_long_axis  = 2/5 * 2*mass_sph *  (outer_radius**5 - inner_radius**5)/ (outer_radius**3 - inner_radius**3)
     I_tot_long_axis            = I_cylinder_long_axis + I_sperical_caps_long_axis    
      
-    ## depending on orientation of cylindrical tank
-    #if:
-        #I[0][0] =
-        #I[1][1] =
-        #I[2][2] =
-    #else:
-        #I[0][0] =
-        #I[1][1] =
-        #I[2][2] =
+
+    # depending on orientation of cylindrical tank     
+    if isinstance(component, RCAIDE.Library.Components.Powertrain.Sources.Fuel_Tanks.Non_Integral_Tank):
+        if component.bwb_aft_tank:
+            I[0][0] = I_tot_long_axis
+            I[1][1] = I_tot_cylin_axis
+            I[2][2] = I_tot_cylin_axis
+        else:
+            I[0][0] = I_tot_cylin_axis
+            I[1][1] = I_tot_long_axis
+            I[2][2] = I_tot_cylin_axis
+    else: 
+        I[0][0] =  I_tot_long_axis
+        I[1][1] =  I_tot_cylin_axis
+        I[2][2] =  I_tot_cylin_axis      
     
     # ----------------------------------------------------------------------------------------------------------------------    
     # transform moment of inertia to the global system
