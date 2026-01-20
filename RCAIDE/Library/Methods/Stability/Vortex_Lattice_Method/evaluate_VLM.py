05bcc0dd719c7dadddc409b1f431a4cf5dea0fc2
ARmolloy <167649645+ARmolloy@users.noreply.github.com>
2026-01-20 10:46:49 -0600

    Update evaluate_VLM.py

--- RCAIDE/Library/Methods/Stability/Vortex_Lattice_Method/evaluate_VLM.py
+++ RCAIDE/Library/Methods/Stability/Vortex_Lattice_Method/evaluate_VLM.py
@@ -8,6 +8,8 @@
 from RCAIDE.Library.Methods.Mass_Properties.Center_of_Gravity.update_center_of_gravity import update_center_of_gravity
 from RCAIDE.Library.Methods.Mass_Properties.Moment_of_Inertia.update_moments_of_inertia import update_moments_of_inertia
 
+import numpy as np
+
 # ----------------------------------------------------------------------------------------------------------------------
 #  Vortex_Lattice
 # ---------------------------------------------------------------------------------------------------------------------- 
@@ -34,7 +36,7 @@ def evaluate(state,settings,vehicle):
      
     # update static margin 
     c_ref                                                   = vehicle.reference_chord   
-    state.conditions.static_stability.static_margin   = (vehicle.neutral_point  - state.conditions.weights.vehicle.global_center_of_gravity[:,0]) / c_ref          
+    state.conditions.static_stability.static_margin   = np.atleast_2d((vehicle.neutral_point  - state.conditions.weights.vehicle.global_center_of_gravity[:,0]) / c_ref).T         
         
     # update moment of inertia
     if settings.update_moments_of_inertia: 
