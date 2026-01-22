ffff6a823ae0d2ab2612023533abc74e67b353ae
ARmolloy <167649645+ARmolloy@users.noreply.github.com>
2026-01-21 22:12:47 -0600

    Update compute_payload_range_diagram.py

--- RCAIDE/Library/Methods/Performance/compute_payload_range_diagram.py
+++ RCAIDE/Library/Methods/Performance/compute_payload_range_diagram.py
@@ -105,7 +105,7 @@ def compute_payload_range_diagram(mission = None, cruise_segment_tag = "cruise",
     for segment in  mission.segments:
         # perform inital weights analysis
         segment.analyses.vehicle.mass_properties.takeoff = None
-        segment.analyses.weights.settings.verbose = False
+        segment.analyses.weights.print_weight_analysis_report = False
     mass_properties(mission)
     vehicle = mission.segments[initial_segment].analyses.vehicle 
   
