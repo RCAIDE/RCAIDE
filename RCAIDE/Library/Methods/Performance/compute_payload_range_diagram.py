76c889ff074faba77a8384ffcc05884465872eaa
Sai Sankalp Shekar <sshekar2@illinois.edu>
2026-02-02 10:18:38 -0600

    Disable CG and inertia analyses in PRD loop
    When computing the payload-range diagram, explicitly disable additional weight analyses by setting run_center_of_gravity_analysis and run_moments_of_inertia_analysis to False for each mission segment. This avoids unnecessary/side-effect computations during the PRD run. Also includes minor formatting/whitespace tweaks around R allocation and update_max_fuel.

--- RCAIDE/Library/Methods/Performance/compute_payload_range_diagram.py
+++ RCAIDE/Library/Methods/Performance/compute_payload_range_diagram.py
@@ -218,10 +218,13 @@ def conventional_payload_range_diagram(vehicle,mission,cruise_segment_tag,fuel_r
     
     # allocating Range array
     R       = [0,0,0]
+    
     for segment in  mission.segments:
         segment.analyses.weights.settings.run_weights_analysis = False
+        segment.analyses.weights.settings.run_center_of_gravity_analysis = False
+        segment.analyses.weights.settings.run_moments_of_inertia_analysis = False
         segment.analyses.geometry.settings.compute_fuel_volume = False
-        segment.analyses.geometry.settings.update_max_fuel     = False
+        segment.analyses.geometry.settings.update_max_fuel  = False
 
 
     # loop for each point of Payload Range Diagram
