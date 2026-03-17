f891381d94e7cda3d27c9ed1875b3b05db06d795
ARmolloy <167649645+ARmolloy@users.noreply.github.com>
2026-03-17 17:03:33 -0400

    Update trimmed_flight_test.py

--- VnV/Verification/analysis_stability/trimmed_flight_test.py
+++ VnV/Verification/analysis_stability/trimmed_flight_test.py
@@ -52,19 +52,19 @@ def main():
     results = missions.base_mission.evaluate() 
 
     elevator_deflection        = results.segments.climb.conditions.control_surfaces.elevator.deflection[0,0] / Units.deg
-    elevator_deflection_true   = -1.2050015164508827
+    elevator_deflection_true   = 0.7173607885939329
     elevator_deflection_diff   = np.abs(elevator_deflection - elevator_deflection_true)
     print('Error1: ',elevator_deflection_diff)
     assert np.abs(elevator_deflection_diff/elevator_deflection_true) < 5e-3
 
     aileron_deflection        = results.segments.climb.conditions.control_surfaces.aileron.deflection[0,0] / Units.deg
-    aileron_deflection_true   = 0.46248573486214606
+    aileron_deflection_true   = 0.44816965172713286
     aileron_deflection_diff   = np.abs(aileron_deflection - aileron_deflection_true)
     print('Error2: ',aileron_deflection_diff)
     assert np.abs(aileron_deflection_diff/aileron_deflection_true) < 5e-3
 
     rudder_deflection        = results.segments.climb.conditions.control_surfaces.rudder.deflection[0,0] / Units.deg
-    rudder_deflection_true   = 1.4111784674575518
+    rudder_deflection_true   = 1.4189996564257998
     rudder_deflection_diff   = np.abs(rudder_deflection - rudder_deflection_true)
     print('Error3: ',rudder_deflection_diff)
     assert np.abs(rudder_deflection_diff/rudder_deflection_true) < 5e-3    
@@ -106,8 +106,8 @@ def base_analysis(vehicle):
     #  Weights
     weights = RCAIDE.Framework.Analyses.Weights.Conventional_General_Aviation() 
     weights.settings.run_weights_analysis = True
-    weights.settings.run_moments_of_inertia = True
-    weights.settings.run_center_of_gravity = True
+    weights.settings.run_moments_of_inertia_analysis = True
+    weights.settings.run_center_of_gravity_analysis = True
     analyses.append(weights) 
 
     # ------------------------------------------------------------------
