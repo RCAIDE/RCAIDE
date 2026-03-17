6c3db324ac48e8eca934fa8d083dffcffc28d289
ARmolloy <167649645+ARmolloy@users.noreply.github.com>
2026-03-17 10:09:34 -0400

    Update compute_fuel_tank_properties.py

--- RCAIDE/Library/Methods/Powertrain/Sources/Fuel_Tanks/compute_fuel_tank_properties.py
+++ RCAIDE/Library/Methods/Powertrain/Sources/Fuel_Tanks/compute_fuel_tank_properties.py
@@ -50,11 +50,7 @@ def compute_fuel_tank_properties(tank,state,distributor):
          
         tank_conditions.boil_off_flow_rate =  m_dot_boil_off 
      
-    #m_0_fuel                                       = state.conditions.weights.components.mass[fuel.tag][0,0] 
     mass_flow_rate                                 = tank.fuel_selector_ratio*distributor_conditions.fuel_mass_flow_rate +  tank_conditions.secondary_mass_flow_rate #tank_conditions.boil_off_flow_rate 
     tank_conditions.mass_flow_rate                 = mass_flow_rate
-    #if len(mass_flow_rate) > 1:
-        # update mass 
-        #state.conditions.weights.components.mass[fuel.tag][:,0]  = m_0_fuel +  np.dot(I, -mass_flow_rate).flatten()
          
     return 
