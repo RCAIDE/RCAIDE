dcc1ec37af4e4e249f63f2fbc7e7664ec1aad57a
Matthew Clarke <mclarke2@stanford.edu>
2026-03-18 12:20:33 -0700

    Update battery_cell.py

--- VnV/Verification/energy_sources/battery_cell.py
+++ VnV/Verification/energy_sources/battery_cell.py
@@ -128,7 +128,7 @@ def lithium_ion_battery_test():
             V_ul_diff   = np.abs(V_ul - V_ul_true[j,i])
             print('Under load voltage difference')
             print(V_ul_diff) 
-            #assert np.abs((V_ul_diff)/V_ul_true[j,i]) < 1e-6  
+            assert np.abs((V_ul_diff)/V_ul_true[j,i]) < 1e-6  
            
             # Temperature Regression
             bat_temp        = results.segments[1].conditions.energy.busses['bus'].battery_modules[battery_chemistry[i]].cell.temperature[2][0]  
@@ -136,7 +136,7 @@ def lithium_ion_battery_test():
             bat_temp_diff   = np.abs(bat_temp  - bat_temp_true[j,i]) 
             print('cell temperature difference')
             print(bat_temp_diff)
-            #assert np.abs((bat_temp_diff)/bat_temp_true[j,i]) < 1e-6
+            assert np.abs((bat_temp_diff)/bat_temp_true[j,i]) < 1e-6
        
             for segment in results.segments.values(): 
                 volts         = segment.conditions.energy.busses['bus'].voltage_under_load[:,0] 
