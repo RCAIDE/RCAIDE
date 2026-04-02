11fa568dbc10f559159dbdbe34b5f86e126de2f9
Matthew Clarke <mclarke2@stanford.edu>
2026-04-02 01:14:28 -0500

    Update Aerodynamics.py

--- RCAIDE/Framework/Analyses/Aerodynamics/Aerodynamics.py
+++ RCAIDE/Framework/Analyses/Aerodynamics/Aerodynamics.py
@@ -67,7 +67,7 @@ class Aerodynamics(Analysis):
 
         self.settings.subsonic                                           = Data() 
         self.settings.subsonic.begin_transonic_rise_mach_number          = 0.65
-        self.settings.subsonic.end_transonic_rise_rise_mach_number       = 0.85  
+        self.settings.subsonic.end_transonic_rise_rise_mach_number       = 0.7 
         self.settings.supersonic                                         = Data() 
         self.settings.supersonic.begin_drag_rise_mach_number             = 0.85
         self.settings.supersonic.end_drag_rise_mach_number               = 1.15     
