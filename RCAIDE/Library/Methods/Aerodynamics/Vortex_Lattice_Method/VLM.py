0631a3c33912cadda09f69c029c34a6948fc9dd0
ARmolloy <167649645+ARmolloy@users.noreply.github.com>
2026-04-09 23:19:10 -0500

    Added in dihedral and Z coordinates to induced drag

--- RCAIDE/Library/Methods/Aerodynamics/Vortex_Lattice_Method/VLM.py
+++ RCAIDE/Library/Methods/Aerodynamics/Vortex_Lattice_Method/VLM.py
@@ -595,13 +595,14 @@ def compute_trefftz_plane_induced_drag(conditions, VD, cl, x_dist, y_dist, z_dis
     # Create centerpoints in body frame
     
     y_control_points = np.stack(np.split(VD.Y[k][::(VD.n_cw[k][0]+1)], divisions_control_point)) #np.stack(np.split(y_dist[0] , divisions))
-    # z_control_points = np.stack(np.split(z_dist[0] , divisions))
-    # x_control_points = np.stack(np.split(x_dist[0] , divisions))
+    z_control_points = np.stack(np.split(VD.Z[k][::(VD.n_cw[k][0]+1)], divisions_control_point))
+    x_control_points = np.stack(np.split(VD.X[k][::(VD.n_cw[k][0]+1)], divisions_control_point))
 
     # Centerpoints in the body frame
     y_centerpoints = np.stack(np.split(y_dist[k], divisions))
-    # z_centerpoints = (z_control_points[:,:-1] + z_control_points[:,1:]) / 2
-    # x_centerpoints = (x_control_points[:,:-1] + x_control_points[:,1:]) / 2
+    z_centerpoints = np.stack(np.split(z_dist[k], divisions))
+    x_centerpoints = np.stack(np.split(x_dist[k], divisions))
+
     symmetric_wing_flags = np.concatenate([np.repeat(np.array(VD.symmetric_wings[0], dtype=bool), 2),np.zeros(np.count_nonzero(~np.array(VD.symmetric_wings[0], dtype=bool)), dtype=bool)])[:n_wings]
 
     shed_vortices = np.zeros_like(y_control_points)
@@ -617,39 +618,23 @@ def compute_trefftz_plane_induced_drag(conditions, VD, cl, x_dist, y_dist, z_dis
             shed_vortices[wing_number,0] = -1*np.sign(y_control_points[wing_number,1]-y_control_points[wing_number,0]) * circulation_dist[wing_number][0]
             shed_vortices[wing_number,-1] = -1*np.sign(y_control_points[wing_number,1]-y_control_points[wing_number,0]) * circulation_dist[wing_number][-1]
             b=0
-
-    # temp = deepcopy(y_centerpoints[0])
-    # temp2 = np.flip(y_centerpoints[1])
-    # y_centerpoints[0] = temp2
-    # y_centerpoints[1] = temp
-
-    # temp = deepcopy(cl_split[0])
-    # temp2 = np.flip(cl_split[1])
-    # cl_split[0] = temp2
-    # cl_split[1] = temp
-
-    # temp = deepcopy(circulation_dist[0])
-    # temp2 = np.flip(circulation_dist[1]) 
-    # circulation_dist[0] = temp2
-    # circulation_dist[1] = temp
-    # shed_vortex_new = np.zeros_like(y_control_points)
-    # shed_vortex_new[1:-1] = np.diff(circulation_dist.ravel())
-    # shed_vortex_new[0] = circulation_dist[0][0]
-    # shed_vortex_new[-1] = -1*circulation_dist[-1][-1]
-    
+    norm_split = np.stack(np.split(VD.normals[k][::(VD.n_cw[k][0])], divisions, axis = 0))
     induced_velocity = np.zeros_like(y_centerpoints)
     for i in range(n_wings):
         for j in range(len(y_centerpoints[i])):
             # Centerpoints:
             for l in range(n_wings):
                 for m in range(len(y_control_points[l])):
-                    distance = y_centerpoints[i][j] - y_control_points[l][m]
+                    distance = np.sqrt((y_centerpoints[i][j] - y_control_points[l][m])**2 + (z_centerpoints[i][j] - z_control_points[l][m])**2)
+                    vortex_direction = 1/distance * np.array([ -1 * (z_centerpoints[i][j] - z_control_points[l][m]),(y_centerpoints[i][j] - y_control_points[l][m])])
+                    vortex_test = 1/distance * np.array([ (y_centerpoints[i][j] - y_control_points[l][m]),  -1 * (z_centerpoints[i][j] - z_control_points[l][m])])
+                    test = 0
+                    v_hat = np.dot(norm_split[i][j][1:], vortex_direction)
+
                     velocity_contribution = shed_vortices[l][m] / (4.0 * np.pi * distance)
-                    induced_velocity[i][j] += velocity_contribution
+                    induced_velocity[i][j] += velocity_contribution * v_hat
     
     alpha_induced_dist = np.arctan(induced_velocity / v_inf)
-    # cd_induced_dist = np.zeros_like(induced_velocity)
-    # for i in range(n_wings):
     cd_induced_dist = alpha_induced_dist * cl_split
     CDi = 0
     for i in range(n_wings):
