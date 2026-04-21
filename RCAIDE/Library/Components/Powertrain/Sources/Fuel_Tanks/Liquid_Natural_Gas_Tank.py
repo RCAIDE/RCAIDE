7f91636276d0fc45110abbd805c858101bb80143
Sidharth Sharma <248633713+sid25harth@users.noreply.github.com>
2026-04-21 18:49:04 -0500

    Update Liquid_Natural_Gas_Tank.py

--- RCAIDE/Library/Components/Powertrain/Sources/Fuel_Tanks/Liquid_Natural_Gas_Tank.py
+++ RCAIDE/Library/Components/Powertrain/Sources/Fuel_Tanks/Liquid_Natural_Gas_Tank.py
@@ -1,6 +1,6 @@
 # RCAIDE/Library/Components/Powertrain/Energy/Sources/Fuel_Tanks/Liquid_Natural_Gas_Tank.py
 # 
-# Created: Aug 2025, S. Shekar
+# Created: April 2026, S. Shekar
 #
 # ----------------------------------------------------------------------------------------------------------------------
 #  IMPORT
@@ -18,11 +18,11 @@ from RCAIDE.Library.Methods.Mass_Properties.Center_of_Gravity  import compute_cy
 from RCAIDE.Library.Methods.Mass_Properties.Moment_of_Inertia  import compute_rounded_end_cylinder_moment_of_inertia, compute_cuboid_moment_of_inertia
 
 # ----------------------------------------------------------------------------------------------------------------------
-#  Liquid Hydrogen Tank
+#  Liquid Natural Gas Tank
 # ---------------------------------------------------------------------------------------------------------------------    
 class Liquid_Natural_Gas_Tank(Non_Integral_Tank):
     """
-    A class representing a non-integral liquid hydrogen (LH₂) fuel tank.  
+    A class representing a non-integral liquid natural gas (LNG) fuel tank.  
 
     Attributes
     ----------
@@ -33,7 +33,7 @@ class Liquid_Natural_Gas_Tank(Non_Integral_Tank):
     insulation_material : Solid
         Insulation material used to reduce heat leak (default: None).  
     design_inlet_temperature : float
-        Nominal inlet temperature of liquid hydrogen [K] (default: 15 K).  
+        Nominal inlet temperature of liquid natural gas [K] (default: 100 K).  
     design_altitude : float
         Design altitude for thermal/structural performance calculations [m].  
     design_isa_deviation : float
@@ -47,14 +47,14 @@ class Liquid_Natural_Gas_Tank(Non_Integral_Tank):
 
     Notes
     -----
-    The liquid hydrogen tank is modeled as a non-integral tank, meaning it is 
+    The liquid natural gas tank is modeled as a non-integral tank, meaning it is 
     not structurally part of the wing or fuselage but rather a dedicated 
     cryogenic vessel. The class supports thermal and structural analyses to 
     capture boil-off, heat leak, and load response.  
 
     **Major Assumptions**
         * Tank shape is cylindrical with optional hemispherical end caps.  
-        * Ullage fraction accounts for vapor volume above liquid hydrogen.  
+        * Ullage fraction accounts for vapor volume above liquid natural gas.  
         * Structural analysis assumes isotropic tank materials.  
         * Thermal analysis assumes steady-state conductive and radiative heat leak.  
 
@@ -75,14 +75,14 @@ class Liquid_Natural_Gas_Tank(Non_Integral_Tank):
     RCAIDE.Library.Methods.Powertrain.Sources.Fuel_Tanks.Non_Integral_Tank.compute_non_integral_tank_volume  
         Equations for computing volume of non-integral tanks.  
     RCAIDE.Library.Methods.Powertrain.Sources.Fuel_Tanks.Liquid_Natural_Gas_Tank.compute_structural_performance  
-        Structural solver for cryogenic hydrogen tanks.  
+        Structural solver for cryogenic natural gas tanks.  
     RCAIDE.Library.Methods.Powertrain.Sources.Fuel_Tanks.Liquid_Natural_Gas_Tank.compute_thermal_performance  
-        Thermal solver for cryogenic hydrogen tanks.  
+        Thermal solver for cryogenic natural gas tanks.  
     """
 
     def __defaults__(self):
         """
-        Set default values for liquid hydrogen tank attributes.  
+        Set default values for liquid natural gas tank attributes.  
 
         Parameters
         ----------
@@ -111,7 +111,7 @@ class Liquid_Natural_Gas_Tank(Non_Integral_Tank):
 
     def compute_volume(self, wings, fuselages,fuel_tanks):
         """
-        Compute the internal volume of the liquid hydrogen tank.  
+        Compute the internal volume of the liquid natural gas tank.  
 
         Determines the non-integral tank volume based on whether it is 
         attached to a wing or configured as an aft BWB tank, and runs 
@@ -139,9 +139,9 @@ class Liquid_Natural_Gas_Tank(Non_Integral_Tank):
         RCAIDE.Library.Methods.Powertrain.Sources.Fuel_Tanks.Non_Integral_Tank.compute_non_integral_tank_volume  
             Equations for computing non-integral tank volumes.  
         RCAIDE.Library.Methods.Powertrain.Sources.Fuel_Tanks.Liquid_Natural_Gas_Tank.compute_structural_performance  
-            Structural solver for cryogenic hydrogen tanks.  
+            Structural solver for cryogenic natural gas tanks.  
         RCAIDE.Library.Methods.Powertrain.Sources.Fuel_Tanks.Liquid_Natural_Gas_Tank.compute_thermal_performance  
-            Thermal solver for cryogenic hydrogen tanks.  
+            Thermal solver for cryogenic natural gas tanks.  
         """
         if self.geometry_type == 'cylindrical':
             if self.wing_tag != None and self.bwb_aft_tank is False:
@@ -156,24 +156,8 @@ class Liquid_Natural_Gas_Tank(Non_Integral_Tank):
                         wing = wings[self.wing_tag]  
                         compute_bwb_aft_tank_volume(self, wing,fuel_tanks)
                         if hasattr(fuel_tanks,self.tag):
-                            compute_cryogenic_cylindrical_tank_volume(self,fuel_tanks)
-        # elif self.geometry_type == 'conformal':
-        #      if self.wing_tag != None and self.bwb_aft_tank is False:
-        #         wing = wings[self.wing_tag]  
-        #         compute_wing_integral_prismatic_tank_volume(self, wing,fuel_tanks)
-        #         if hasattr(fuel_tanks,self.tag):
-        #             compute_liquid_hydrogen_tank_conformal_volume(self,fuel_tanks)
-        #      else:
-        #         if self.bwb_aft_tank == True:
-        #             if self.wing_tag != None:
-        #                 wing = wings[self.wing_tag]  
-        #                 compute_bwb_aft_integral_prismatic_tank_volume(self, wing,fuel_tanks)
-        #                 if hasattr(fuel_tanks,self.tag):
-        #                     compute_liquid_hydrogen_tank_conformal_volume(self,fuel_tanks)
-        else:
-            raise NotImplementedError
-
-                        
+                            compute_cryogenic_cylindrical_tank_volume(self,fuel_tanks)        
+        
         return
   
     def compute_moments_of_inertia(self,vehicle,center_of_gravity=[[0, 0, 0]]): 
@@ -199,11 +183,6 @@ class Liquid_Natural_Gas_Tank(Non_Integral_Tank):
         if  self.geometry_type == 'cylindrical':
             inner_radius = self.inner_structure.inner_diameter/2
             _, _ = compute_rounded_end_cylinder_moment_of_inertia(self, outer_length,outer_radius,inner_length=inner_length, inner_radius=inner_radius, center_of_gravity=center_of_gravity, fuel_tank=True) 
-        elif self.geometry_type == 'conformal' and self.bwb_aft_tank:
-            pass
-        elif self.geometry_type == 'conformal' and self.bwb_aft_tank == False:
-            thickness = self.inner_structure.thickness + self.insulation_thickness
-            _, _ = compute_cuboid_moment_of_inertia(self, outer_length = self.average_outer_length, outer_width = self.average_outer_width, outer_height = self.average_outer_height, inner_length = self.average_outer_length - 2 *thickness, inner_width = self.average_outer_width - 2*thickness, inner_height=self.average_outer_height - 2 * thickness, center_of_gravity=center_of_gravity, fuel_tank=True)
         
         return
     
@@ -225,6 +204,4 @@ class Liquid_Natural_Gas_Tank(Non_Integral_Tank):
         if self.geometry_type == 'cylindrical':        
             length = self.lengths.external +  self.diameters.external
             _      = compute_cylinder_center_of_gravity(self, length )
-        elif self.geometry_type == 'conformal':
-            pass # cg calcs are done and stored on the fuel tank during volume computations
         return
