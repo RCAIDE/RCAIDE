8d1e6e77d6af64a1672e236019f895b65e6fdf69
Sai Sankalp Shekar <sshekar2@illinois.edu>
2026-04-23 12:27:44 -0500

    Update digital_elevation_test.py

--- VnV/Verification/analysis_aeroacoustics/digital_elevation_test.py
+++ VnV/Verification/analysis_aeroacoustics/digital_elevation_test.py
@@ -63,7 +63,7 @@ def main():
     plot_battery_pack_conditions(results) 
 
     X57_SPL        = np.max(results.segments.cruise.conditions.aeroacoustics.hemisphere_SPL_dBA) 
-    X57_SPL_true   = 80.32077228241845
+    X57_SPL_true   = 86.04345918426637
     X57_diff_SPL   = np.abs(X57_SPL - X57_SPL_true)
     print('Error: ',X57_diff_SPL)
     assert np.abs((X57_SPL - X57_SPL_true)/X57_SPL_true) < 1e-3 
@@ -120,8 +120,6 @@ def base_analysis(vehicle):
       
     #  Aerodynamics Analysis
     aerodynamics          = RCAIDE.Framework.Analyses.Aerodynamics.Vortex_Lattice_Method()  
-    aerodynamics.settings.number_of_spanwise_vortices   = 5
-    aerodynamics.settings.number_of_chordwise_vortices  = 2   
     analyses.append(aerodynamics)   
 
     # ------------------------------------------------------------------
